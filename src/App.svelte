97ba983c02accb59b6753c7fdb6c8204c15f27ef
Benjamin Haber <benjamin@Benjamins-MacBook-Pro.local>
2020-10-22 14:24:46 -0700

    added ability to change work hours

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@
   import _ from "lodash";
 
   let zones: string[] = [];
+  let start_of_day = 9;
+  let end_of_day = 17;
 
   const select = (event: any) => {
     const zone = event.target.value;
@@ -10,23 +12,24 @@
     zones = [...zones, zone];
   };
 
-  const getColor = (hour: number) => {
-    if (hour < 6) return "red";
-    if (hour < 9) return "orange";
-    if (hour < 18) return "green";
-    if (hour < 21) return "orange";
+  $: getColor = (time: moment.Moment) => {
+    const hour = time.hours();
+    if (hour < start_of_day - 3) return "red";
+    if (hour < start_of_day) return "orange";
+    if (hour <= end_of_day) return "green";
+    if (hour <= end_of_day + 3) return "orange";
     return "red";
   };
 
   const getHoursForZone = (zone: string) => {
     const zero = moment().startOf("d").tz(zone);
-    return _.times(24, (i) => (zero.hours() + i) % 24);
+    return _.times(24, (i) => zero.clone().add(i, "h"));
   };
 
+  // MODIFY ZONES FUNCTION
   const remove = (zone: string) => {
     zones = zones.filter((z) => z !== zone);
   };
-
   const swap = (index: number, target: number) => {
     if (target < 0 || target >= zones.length) return;
 
@@ -34,7 +37,6 @@
     zones[target] = zones[index];
     zones[index] = b;
   };
-
   const reset = () => {
     zones = [moment.tz.guess()];
   };
@@ -42,34 +44,60 @@
   onMount: reset();
 </script>
 
-<button class="reset" on:click={reset}>Reset</button>
+<section aria-label="Controls" class="pt2 pb3 ph2">
+  <button class="reset" on:click={reset}>Reset</button>
 
-<!-- svelte-ignore a11y-no-onchange -->
-<select on:change={select}>
-  {#each moment.tz.names() as zone}
-    <option value={zone}>{zone}</option>
-  {/each}
-</select>
-
-<div class="flex flex-row">
-  {#each zones as zone, index}
-    <div class="flex flex-column w4 br">
-      <div class="pa2 bb">
-        <p class="truncate tc">{zone}</p>
-        <p class="tc">({moment.tz(zone).format('Z')})</p>
-        <button class="pointer" on:click={() => remove(zone)}>X</button>
-        <button
-          class="pointer"
-          on:click={() => swap(index, index - 1)}>{'<'}</button>
-        <button
-          class="pointer"
-          on:click={() => swap(index, index + 1)}>{'>'}</button>
-      </div>
-      {#each getHoursForZone(zone) as hour}
-        <div class="bb tc pa1" style={`background-color:${getColor(hour)}`}>
-          {moment().hours(hour).format('h az')}
-        </div>
+  <div class="dib ml3">
+    <label for="timezone" class="db">Add Time Zone</label>
+    <!-- svelte-ignore a11y-no-onchange -->
+    <select name="timezone" class="db" on:change={select}>
+      {#each moment.tz.names() as zone}
+        <option value={zone}>{zone}</option>
       {/each}
-    </div>
-  {/each}
-</div>
+    </select>
+  </div>
+
+  <!-- svelte-ignore a11y-no-onchange -->
+  <div class="dib">
+    <label for="start" class="db">Start</label>
+    <select name="start" bind:value={start_of_day}>
+      {#each _.times(24) as hour}
+        <option value={hour}>{moment().hours(hour).format('h a')}</option>
+      {/each}
+    </select>
+  </div>
+  <div class="dib">
+    <label for="end" class="db">End</label>
+    <!-- svelte-ignore a11y-no-onchange -->
+    <select name="end" class="db" bind:value={end_of_day}>
+      {#each _.times(24) as hour}
+        <option value={hour}>{moment().hours(hour).format('h a')}</option>
+      {/each}
+    </select>
+  </div>
+</section>
+
+<section aria-label="Compare">
+  <div class="flex flex-row bt">
+    {#each zones as zone, index}
+      <div class="flex flex-column w4 br">
+        <div class="pa2 bb">
+          <p class="truncate tc mb0 mt0" title={zone}>{zone}</p>
+          <p class="tc mt0">({moment.tz(zone).format('Z')})</p>
+          <button
+            class="pointer"
+            on:click={() => swap(index, index - 1)}>{'<'}</button>
+          <button
+            class="pointer"
+            on:click={() => swap(index, index + 1)}>{'>'}</button>
+          <button class="pointer" on:click={() => remove(zone)}>X</button>
+        </div>
+        {#each getHoursForZone(zone) as hour}
+          <div class="bb tc pa1" style={`background-color:${getColor(hour)}`}>
+            {hour.format('h a')}
+          </div>
+        {/each}
+      </div>
+    {/each}
+  </div>
+</section>
